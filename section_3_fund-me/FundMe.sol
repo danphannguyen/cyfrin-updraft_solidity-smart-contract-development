18a5785e9bb33c853a1fa92aa4e26c0a1a49a315
danphannguyen <77532827+danphannguyen@users.noreply.github.com>
2026-07-18 23:17:13 +0200

    12.0 Getting Real World Price Data From Chainlink

--- section_3_fund-me/FundMe.sol
+++ section_3_fund-me/FundMe.sol
@@ -5,6 +5,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts@1.5.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
 
     // uint256 public myValue = 1;
@@ -24,7 +26,26 @@ contract FundMe {
         // Undo any actions that have been done, and send the remaining gas back
     }
 
-    function withdraw() public {}
-}
+    // function withdraw() public {}
+    function getPrice() public view returns (uint256) {
+        // Instanciate the contrat with the interface and the oracle adress to help our IDE with functions he don't know(ETH/USD on Sepolia)
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        
+        // Call the oracle function latestRoundData()
+        // We can also write : (,int256 price,,,) = priceFeed.latestRoundData();
+        (
+            /* uint80 roundID */,
+            int256 price,
+            /* uint startedAt */,
+            /* uint timeStamp */,
+            /* uint80 answeredInRound */
+        ) = priceFeed.latestRoundData();
+        
+        // 3. The price is return with 8 decimals. Msg.value with 18 decimals (Wei).
+        // Multiply by 1e10 to get 18 decimals in total and convert in uint256.
+        return uint256(price * 1e10);
+    }
+        function getCOnversionRate() public {}
+    }
 
 // Failed transaction will spend gas
